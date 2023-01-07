fe4352d8e590772a14df2e8c6e6baae7486e23a9
Fábio Marane <fabiomarane@gmail.com>
2023-01-07 02:39:56 -0300

    Changes

--- Python/1 - FIBONACCI SEQUENCE GENERATOR.py
+++ Python/1 - FIBONACCI SEQUENCE GENERATOR.py
@@ -1,12 +1,12 @@
-# This function gives the first N Fibonacci numbers, based on the user's input.
+#This function gives the first N Fibonacci numbers, based on the user's input.
 
-def fibonacci(fibonacci = [0, 1]):
-    N = int(input('Fibonacci sequence until the N-th number.\n N = '))
-    while N < 0:
-        N = input(int('Invalid number. Only positive numbers. Try again'))
-    while len(fibonacci) <= N:
-        fibonacci.append(fibonacci[-1] + fibonacci[-2])
-    fibonacci.pop(0)
+def fibonacci(N = int, fibonacci = [0, 1]):    #Definition of the amount of Fibonacci Numbers, and the fibonacci list itself.
+    N = int(input('Fibonacci sequence until the N-th number.\n N = '))    #Constraint to natural numbers
+    while N < 0:    #User input for N, defined previousy
+        N = input(int('Invalid number. Only positive numbers. Try again'))  
+    while len(fibonacci) <= N:    #Constraint to the list size
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])     #Appending the numbers, according to the Fibonacci Sequence
+    fibonacci.pop(0)     #Removal of the number zero
     return print("Here's the first {} Fibonacci numbers:\n {}".format(N, fibonacci))
 
 fibonacci()
--- Python/2 - TOTAL SUM CALCULATOR.py
+++ Python/2 - TOTAL SUM CALCULATOR.py
@@ -1,12 +1,12 @@
 #The function will sum all the numbers given by the user, until the number zero - the flag - is written. It DOES NOT recognizes alphabetic characters, and will not work if the user inputs it. A more refined program is available at the next file: "Average Value Calculator"
 
-def Sum(n = float, list = []):
+def Sum(n = float, list = []):      #Definition of the variable n for receiving the user numbers, and the list to store them
     print('Write as many numbers as you want to sum. When you decide to stop, just write the number "0".')
-    while n != 0:
-        n = float(input('Number: '))
-        list.append(n)
+    while n != 0:       #Constraint to the flag-element
+        n = float(input('Number: '))        #User input for the desired numbers to sum
+        list.append(n)      #Appending the numbers to the previously defined list
 
-    s = sum(list)
+    s = sum(list)       #Summing all elements of the list for the desired result
     return(
         print('The sum of the given numbers equals to {}'.format(s))
     )
